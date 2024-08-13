af2248786725b3ea2e3ac15b88da5f72ce9ba8b3
Gerson2102 <gersonloavas@gmail.com>
2024-08-12 19:11:42 -0600

    primitive types section completed

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,11 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let mut a = Vec::<u16>::new();
+    let mut i: u16 = 0;
+    while i <= 100 {
+        a.push(i);
+        i += 1;
+    }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
